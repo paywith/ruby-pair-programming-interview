64298247ce395b81b2497c821f6ce0d57aeb9d88
Vitor Oliveira <vbrazo@gmail.com>
2021-08-10 13:20:25 -0700

    Remove unnecessary file

--- spec/support/request_helpers.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module RequestsHelpers
-  def headers(response)
-    { "Authorization": response.header["Authorization"], "Content-Type": "application/json" }
-  end
-end
